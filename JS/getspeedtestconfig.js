d5db40ac6eb5635b8a691e5b870fae87fa965f8c
tripall <b155073@gmail.com>
2020-05-05 15:11:38 +0800

    Disable the HTTP get at the home

--- JS/getspeedtestconfig.js
+++ JS/getspeedtestconfig.js
@@ -1,2 +1,10 @@
-let url = 'https://www.speedtest.net/api/ios-config.php';
-$done($httpClient.get(url));
+let ssidMatched = ($network.wifi.ssid === 'Kun_Dorm_5G' || $network.wifi.ssid === 'Kun_Dorm');
+if (ssidMatched)
+{
+  $done();
+}
+else
+{
+  let url = 'https://www.speedtest.net/api/ios-config.php';
+  $done($httpClient.get(url));
+}
