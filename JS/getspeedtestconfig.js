c7f90212f39e639010f1f07644ad83af5a802f32
tripall <b155073@gmail.com>
2020-05-13 23:08:52 +0800

    update the script only apply in the cellular network

--- JS/getspeedtestconfig.js
+++ JS/getspeedtestconfig.js
@@ -1,10 +1,10 @@
-let inWifi = ($network.v4.primaryInterface === "en0");
-if (inWifi)
-{
-  $done();
-}
-else
+let inCellular = ($network.v4.primaryInterface === "pdp_ip0");
+if (inCellular)
 {
   let url = 'https://www.speedtest.net/api/ios-config.php';
   $done($httpClient.get(url));
+}
+else
+{
+  $done();
 }
